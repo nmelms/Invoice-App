e5cd0cb8e656d8dbbce05b5c46c36ab4865a6b2e
Nick Melms <nmelms92@gmail.com>
2022-07-14 00:48:23 -0400

    styled a new button

--- src/components/NewButton.js
+++ src/components/NewButton.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import plusIcon from '../assets/icon-plus.svg'
+import React from "react";
+import plusIcon from "../assets/icon-plus.svg";
 
 export default function NewButton({ setPage }) {
   return (
-    <button onClick={() => setPage('newInvoice')} className="NewButton">
-      <img src={plusIcon} />
-      <h2>New Invoice</h2>
+    <button onClick={() => setPage("newInvoice")} className="NewButton">
+      <div className="plusIcon">
+        <img className="plusIconImg" src={plusIcon} />
+      </div>
+      <div className="newButtonText">
+        <p>New</p>
+      </div>
     </button>
-  )
+  );
 }
