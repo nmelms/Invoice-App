ccad576a648af1d37a2a60d1667cf7715487ea86
Nick Melms <nmelms92@gmail.com>
2022-07-25 00:38:58 -0400

    styled viewInvoice for desktop

--- src/components/ViewInvoice.js
+++ src/components/ViewInvoice.js
@@ -90,114 +90,116 @@ export default function ViewInvoice({ setPage, page }) {
   };
 
   return (
-    <div className="viewInvoice">
+    <>
       <NavBar />
       <BackButton setPage={setPage} name="home" />
-      {alert && <Alert setUserResponse={setUserResponse} />}
+      <div className="viewInvoice">
+        {alert && <Alert setUserResponse={setUserResponse} />}
 
-      <div className="invoiceStatus ">
-        <div className="statusAndName">
-          <p>status</p>
-          <p
-            className="status"
-            style={{
-              fontStyle: "bold",
-              background: `rgba(${color}, 0.1)`,
-              color: `rgba(${color})`,
-            }}
-          >
-            {item.status}
-          </p>
+        <div className="invoiceStatus ">
+          <div className="statusAndName">
+            <p>status</p>
+            <p
+              className="status"
+              style={{
+                fontStyle: "bold",
+                background: `rgba(${color}, 0.1)`,
+                color: `rgba(${color})`,
+              }}
+            >
+              {item.status}
+            </p>
+          </div>
+
+          <div className="invoiceStatusButtons">
+            <button className="editBtn" onClick={() => handleEditClick()}>
+              <h4>edit</h4>
+            </button>
+            <button className="deleteBtn" onClick={() => onDeleteClick()}>
+              <h4>delete</h4>
+            </button>
+            <button className="paidBtn" onClick={() => handleStatusClick()}>
+              <h4>mark as paid</h4>
+            </button>
+          </div>
         </div>
 
-        <div className="invoiceStatusButtons">
-          <button className="editBtn" onClick={() => handleEditClick()}>
-            <h4>edit</h4>
-          </button>
-          <button className="deleteBtn" onClick={() => onDeleteClick()}>
-            <h4>delete</h4>
-          </button>
-          <button className="paidBtn" onClick={() => handleStatusClick()}>
-            <h4>mark as paid</h4>
-          </button>
+        <div className="viewInvoiceBody">
+          <div className="tagAndProdDes">
+            <h4>
+              <h4 style={{ color: "#7E88C3" }}>#</h4>
+              {item.tag}
+            </h4>
+            <p>{item.prodDes}</p>
+          </div>
+          <div className="senderAddress">
+            <p>{item.street}</p>
+            <p>{item.city}</p>
+            <p>{item.state}</p>
+            <p>{item.zip}</p>
+            <p>{item.country}</p>
+          </div>
+
+          <div className="invoiceDate">
+            <p>invoice Date</p>
+            <h4>{item.invoiceDate}</h4>
+          </div>
+          <div className="billToAddress">
+            <p>bill To</p>
+            <h4>{item.clientsName}</h4>
+            <p>{item.cstreet}</p>
+            <p>{item.ccity}</p>
+            <p>{item.cstate}</p>
+            <p>{item.czip}</p>
+            <p>{item.ccountry}</p>
+          </div>
+          <div className="paymentDue">
+            <p className="paymentDueP">Payment Due</p>
+            <h4>{item.dueDate}</h4>
+          </div>
+
+          <div className="sentTo">
+            <p>Sent To</p>
+            <h4>{item.clientsEmail}</h4>
+          </div>
+          <div className="invoiceItemsWrapper">
+            {item.items.map((item, index) => {
+              item.total = item.qty * item.price;
+              grandTotal += item.total;
+              return (
+                <div key={index} className="invoiceItems">
+                  <div className="invoiceItemName">
+                    <h4>{item.itemName}</h4>
+                  </div>
+                  <div className="itemMultiply">
+                    <p>
+                      {item.qty} X ${item.price}
+                    </p>
+                  </div>
+                  <div className="invoiceItemTotal">${item.total}</div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="grandTotal">
+            <p>Grand Total</p>
+            <h2>${grandTotal}</h2>
+          </div>
+
+          <div className="viewInvoiceButtons">
+            <button className="editBtn" onClick={() => handleEditClick()}>
+              <h4>edit</h4>
+            </button>
+            <button className="deleteBtn" onClick={() => onDeleteClick()}>
+              <h4>delete</h4>
+            </button>
+            <button className="paidBtn" onClick={() => handleStatusClick()}>
+              <h4>mark as paid</h4>
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="viewInvoiceBody">
-        <div className="tagAndProdDes">
-          <h4>
-            <h4 style={{ color: "#7E88C3" }}>#</h4>
-            {item.tag}
-          </h4>
-          <p>{item.prodDes}</p>
-        </div>
-        <div className="senderAddress">
-          <p>{item.street}</p>
-          <p>{item.city}</p>
-          <p>{item.state}</p>
-          <p>{item.zip}</p>
-          <p>{item.country}</p>
-        </div>
-
-        <div className="invoiceDate">
-          <p>invoice Date</p>
-          <h4>{item.invoiceDate}</h4>
-        </div>
-        <div className="billToAddress">
-          <p>bill To</p>
-          <h4>{item.clientsName}</h4>
-          <p>{item.cstreet}</p>
-          <p>{item.ccity}</p>
-          <p>{item.cstate}</p>
-          <p>{item.czip}</p>
-          <p>{item.ccountry}</p>
-        </div>
-        <div className="paymentDue">
-          <p className="paymentDueP">Payment Due</p>
-          <h4>{item.dueDate}</h4>
-        </div>
-
-        <div className="sentTo">
-          <p>Sent To</p>
-          <h4>{item.clientsEmail}</h4>
-        </div>
-        <div className="invoiceItemsWrapper">
-          {item.items.map((item, index) => {
-            item.total = item.qty * item.price;
-            grandTotal += item.total;
-            return (
-              <div key={index} className="invoiceItems">
-                <div className="invoiceItemName">
-                  <h4>{item.itemName}</h4>
-                </div>
-                <div className="itemMultiply">
-                  <p>
-                    {item.qty} X ${item.price}
-                  </p>
-                </div>
-                <div className="invoiceItemTotal">${item.total}</div>
-              </div>
-            );
-          })}
-        </div>
-
-        <div className="grandTotal">
-          <p>Grand Total</p>
-          <h2>${grandTotal}</h2>
-        </div>
-
-        <div className="viewInvoiceButtons">
-          <button className="editBtn" onClick={() => handleEditClick()}>
-            <h4>edit</h4>
-          </button>
-          <button className="deleteBtn" onClick={() => onDeleteClick()}>
-            <h4>delete</h4>
-          </button>
-          <button className="paidBtn" onClick={() => handleStatusClick()}>
-            <h4>mark as paid</h4>
-          </button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
