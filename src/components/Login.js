ca7d56c2affeec001e0a75721da68c7624424b78
Nick Melms <nmelms92@gmail.com>
2022-11-05 13:47:44 -0400

    styled basic login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,11 @@ import React, { useContext, useEffect, useState } from "react";
 import GlobalContext from "../GlobalContext";
 import { auth, logInWithEmailAndPassword, signInWithGoogle } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import userIcon from "../assets/user.png";
 
 export default function Login({ setPage }) {
   const [email, setEmail] = useState("");
+
   const [password, setPassword] = useState("");
   const [user, loading, error] = useAuthState(auth);
   const { newUser, setNewUser } = useContext(GlobalContext);
@@ -21,8 +23,9 @@ export default function Login({ setPage }) {
   }, [user, loading]);
 
   return (
-    <div className="login">
+    <div className="loginPage">
       <div className="loginWrapper">
+        <img className="userIcon" src={userIcon} />
         <div className="emailAndPassword">
           <input
             type="text"
@@ -36,14 +39,20 @@ export default function Login({ setPage }) {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button
-            onClick={() => logInWithEmailAndPassword(email, password)}
-            className="loginBtn"
-          >
-            Sign In
-          </button>
-          <button className="loginBtn">Sign up</button>
         </div>
+        <ul className="loginButtons">
+          <li>
+            <button
+              onClick={() => logInWithEmailAndPassword(email, password)}
+              className="loginBtn"
+            >
+              Sign In
+            </button>
+          </li>
+          <li>
+            <button className="loginBtn">Sign up</button>
+          </li>
+        </ul>
       </div>
     </div>
   );
